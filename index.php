5fc55642e0053e6c60f760200cc589c3293fc2d7
Srikar KSV <srikar1awesome@gmail.com>
2021-11-27 23:07:33 +0530

    👌 IMPROVE: Prototyped webapp to accept expiry date

--- index.php
+++ index.php
@@ -3,14 +3,28 @@ include "templates/header.php";
 require "credentials.php";
 
 $shortened_url = "";
-
+date_default_timezone_set("Asia/Calcutta");   //India time (GMT+5:30)
 if (isset($_GET["id"])) {
   $conn = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASENAME);
   $id = $_GET["id"];
   $select_sql = "SELECT * FROM url WHERE id='$id'";
   $result = mysqli_query($conn, $select_sql);
   $shortened_url = mysqli_fetch_assoc($result);
-  header("Location: {$shortened_url['url']}");
+
+  $now = new DateTime("now");
+  $expiry_date = new DateTime($shortened_url["expiryDate"]);
+
+  if ($expiry_date == NULL || $now < $expiry_date) {
+    header("Location: {$shortened_url['url']}");
+  } else {
+    $delete_sql = "DELETE FROM url WHERE id='$id'";
+    if (mysqli_query($conn, $delete_sql)) {
+    } else {
+      echo 'Query error: ' . mysqli_error($conn);
+    }
+    echo "Shortened link expired!";
+  }
+
 
   mysqli_close($conn);
 }
@@ -18,12 +32,18 @@ if (isset($_GET["id"])) {
 if (isset($_POST["submit"])) {
   $conn = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASENAME);
   $url = $_POST["url"];
+  $expiry_date = NULL;
+  $expiry_date_set = $_POST["expiryDate"];
+  echo "<p>Expiry date: $expiry_date_set</p>";
+
+  if ($expiry_date_set == "custom") {
+    $expiry_date = $_POST["customDate"];
+  }
 
 
   $bytes = random_bytes(5);
   $generated_id = bin2hex($bytes);
-  $insert_sql = "INSERT INTO url VALUES('$generated_id', '$url', NULL)";
-
+  $insert_sql = $expiry_date == NULL ? "INSERT INTO url VALUES('$generated_id', '$url', NULL)" : "INSERT INTO url VALUES('$generated_id', '$url', '$expiry_date')";
   if (mysqli_query($conn, $insert_sql)) {
   } else {
     echo 'Query error: ' . mysqli_error($conn);
@@ -39,6 +59,15 @@ if (isset($_POST["submit"])) {
 ?>
 
 <title>Shorty | URL shortner</title>
+<style>
+  input[type="datetime-local"] {
+    display: none;
+  }
+
+  input#custom:checked+input[type="datetime-local"] {
+    display: block;
+  }
+</style>
 
 <body>
   <main>
@@ -47,6 +76,12 @@ if (isset($_POST["submit"])) {
     <form action="index.php" method="post">
       <label for="url">URL</label>
       <input type="text" name="url" id="url">
+      <p>Set expiry date for the shortened link</p>
+      <label for="permanent">Permanent</label>
+      <input type="radio" name="expiryDate" id="permanent" value="permanent">
+      <label for="custom">Custom</label>
+      <input type="radio" name="expiryDate" id="custom" value="custom">
+      <input type="datetime-local" name="customDate">
       <input type="submit" name="submit" value="Shorten">
     </form>
 
