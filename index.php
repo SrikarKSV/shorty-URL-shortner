df1a5f6c724104478e2a03c4273cb56d2bedc655
Srikar KSV <srikar1awesome@gmail.com>
2021-12-01 16:40:38 +0530

    📦 NEW: Changed db helper file name

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+$title = "Shorty | URL shortner";
+$css_path = "public/css/main.css";
 include "templates/header.php";
-require "credentials.php";
+require "db_helper.php";
 
 $shortened_url = "";
 date_default_timezone_set("Asia/Calcutta");   //India time (GMT+5:30)
 if (isset($_GET["id"])) {
-  $conn = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASENAME);
   $id = $_GET["id"];
   $select_sql = "SELECT * FROM url WHERE id='$id'";
   $result = mysqli_query($conn, $select_sql);
@@ -58,7 +59,6 @@ if (isset($_POST["submit"])) {
 }
 ?>
 
-<title>Shorty | URL shortner</title>
 <style>
   input[type="datetime-local"] {
     display: none;
@@ -69,33 +69,32 @@ if (isset($_POST["submit"])) {
   }
 </style>
 
-<body>
-  <main>
-    <h1>Shorten URL</h1>
 
-    <form action="index.php" method="post">
-      <label for="url">URL</label>
-      <input type="text" name="url" id="url">
-      <p>Set expiry date for the shortened link</p>
-      <label for="permanent">Permanent</label>
-      <input type="radio" name="expiryDate" id="permanent" value="permanent">
-      <label for="custom">Custom</label>
-      <input type="radio" name="expiryDate" id="custom" value="custom">
-      <input type="datetime-local" name="customDate">
-      <input type="submit" name="submit" value="Shorten">
-    </form>
+<main>
+
+  <form action="index.php" method="post">
+    <label for="url">URL</label>
+    <input type="text" name="url" id="url">
+    <p>Set expiry date for the shortened link</p>
+    <label for="permanent">Permanent</label>
+    <input type="radio" name="expiryDate" id="permanent" value="permanent">
+    <label for="custom">Custom</label>
+    <input type="radio" name="expiryDate" id="custom" value="custom">
+    <input type="datetime-local" name="customDate">
+    <input type="submit" name="submit" value="Shorten">
+  </form>
 
 
-    <?php
-    if ($shortened_url) {
-    ?>
-      <p>URL is shortened: <a href="<?php echo "http://localhost/shorty-url-shortner?id={$shortened_url['id']}" ?>"><?php echo "http://localhost/shorty-url-shortner?id={$shortened_url['id']}" ?></a></p>
-    <?php } ?>
-  </main>
+  <?php
+  if ($shortened_url) {
+  ?>
+    <p>URL is shortened: <a href="<?php echo "http://localhost/shorty-url-shortner?id={$shortened_url['id']}" ?>"><?php echo "http://localhost/shorty-url-shortner?id={$shortened_url['id']}" ?></a></p>
+  <?php } ?>
+</main>
 
-  <footer>
-    <p>Made with ❤️ by Akash and Srikar</p>
-  </footer>
+<footer>
+  <p>Made with ❤️ by Akash and Srikar</p>
+</footer>
 </body>
 
 </html>
