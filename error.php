c0830ea7cfa579d54dca2b36082e465e6e50402c
Srikar KSV <srikar1awesome@gmail.com>
2021-12-02 23:12:28 +0530

    📦 NEW: Added new error code

--- error.php
+++ error.php
@@ -1,10 +1,12 @@
 <?php
-$title = "Error | URL shortner";
+$title = "{$_GET["error"]} error | URL shortner";
 $css_path = "./public/css/main.css";
 include "./templates/header.php";
 
 if (isset($_GET["error"])) {
-  if ($_GET["error"] == "404") {
+  if ($_GET["error"] == "403") {
+    echo "You cannot access this page";
+  } else if ($_GET["error"] == "404") {
     echo "Requested shortened link is either expired or invalid.";
   } else if ($_GET["error"] == "500") {
     echo "There was an error on server try again later.";
