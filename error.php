03da9bc81f7cb3fbe714a8763b1a8d36a64f018f
Srikar KSV <srikar1awesome@gmail.com>
2021-12-03 18:43:48 +0530

    👌 IMPROVE: Styled error page with CSS

--- error.php
+++ error.php
@@ -1,18 +1,27 @@
 <?php
 $title = "{$_GET["error"]} error | URL shortner";
-$css_path = "./public/css/main.css";
+$css_path = "./public/css/style.css";
 include "./templates/header.php";
+?>
 
-if (isset($_GET["error"])) {
-  if ($_GET["error"] == "403") {
-    echo "You cannot access this page";
-  } else if ($_GET["error"] == "404") {
-    echo "Requested shortened link is either expired or invalid.";
-  } else if ($_GET["error"] == "500") {
-    echo "There was an error on server try again later.";
-  } else {
-    header("location: index.php");
-  }
-} else {
-  header("location: index.php");
-}
+<main>
+  <div class="form-container">
+    <h2><?php echo $_GET["error"]; ?> error</h2>
+    <?php
+    if (isset($_GET["error"])) {
+      if ($_GET["error"] == "403") {
+        echo "<h5>You cannot access this page</h5>";
+      } else if ($_GET["error"] == "404") {
+        echo "<h5>Requested shortened link is either expired or invalid.</h5>";
+      } else if ($_GET["error"] == "500") {
+        echo "<h5>There was an error on server try again later.</h5>";
+      } else {
+        header("location: index.php");
+      }
+    } else {
+      header("location: index.php");
+    }
+
+    ?>
+  </div>
+</main>
