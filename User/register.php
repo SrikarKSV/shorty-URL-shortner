e319dde42f4cebd9f840f1d7fdb36634281a2496
Srikar KSV <srikar1awesome@gmail.com>
2021-12-03 16:29:10 +0530

    👌 IMPROVE: Designed login and register with CSS

--- User/register.php
+++ User/register.php
@@ -1,6 +1,6 @@
 <?php
 $title = "Register | URL shortner";
-$css_path = "../public/css/main.css";
+$css_path = "../public/css/style.css";
 $login_route = "./login.php";
 $register_route = "./register.php";
 include "../templates/header.php";
@@ -38,8 +38,6 @@ if (isset($_POST["submit"])) {
       mysqli_close($conn);
       exit();
     }
-
-    mysqli_close($conn);
   }
 
   function createUser($conn, $email, $username, $pwd)
@@ -62,32 +60,35 @@ if (isset($_POST["submit"])) {
 ?>
 
 <main>
-  <form class="register-form" action="register.php" method="POST">
-    <?php if (isset($_GET["error"]) && $_GET["error"] == "usernametaken") {
-      echo "<span class='usernametaken show'>Username already taken, try another.</span>";
-    } ?>
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email">
-    <span class="enter-email">Enter your email</span>
-    <span class="invalid-email">Enter valid email</span>
+  <div class="form-container medium-size">
+    <h2>Register</h2>
+    <form class="register-form" action="register.php" method="POST">
+      <?php if (isset($_GET["error"]) && $_GET["error"] == "usernametaken") {
+        echo "<span class='usernametaken show'>Username already taken, try another.</span>";
+      } ?>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email">
+      <span class="enter-email">Enter your email</span>
+      <span class="invalid-email">Enter valid email</span>
 
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username">
-    <span class="enter-username">Enter your Username</span>
-    <span class="invalid-username">Enter valid Username(It can only have letters & numbers)</span>
+      <label for="username">Username</label>
+      <input type="text" name="username" id="username">
+      <span class="enter-username">Enter your Username</span>
+      <span class="invalid-username">Enter valid Username(It can only have letters & numbers)</span>
 
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
-    <span class="enter-password">Enter your Password</span>
-    <span class="invalid-password">Enter valid Password(It can only have letters, spaces & numbers)</span>
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password">
+      <span class="enter-password">Enter your Password</span>
+      <span class="invalid-password">Enter valid Password(It can only have letters, spaces & numbers)</span>
 
-    <label for="repeat-password">Repeat password</label>
-    <input type="password" name="repeatPassword" id="repeatPassword">
-    <span class="enter-rpassword">Confirm your password</span>
-    <span class="invalid-rpassword">Both password don't match</span>
+      <label for="repeatPassword">Repeat password</label>
+      <input type="password" name="repeatPassword" id="repeatPassword">
+      <span class="enter-rpassword">Confirm your password</span>
+      <span class="invalid-rpassword">Both password don't match</span>
 
-    <input type="submit" name="submit" value="Sign up">
-  </form>
+      <input class="btn" type="submit" name="submit" value="Sign up">
+    </form>
+  </div>
 </main>
 
 <?php
